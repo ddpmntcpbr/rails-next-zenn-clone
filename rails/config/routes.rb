c608848f47f23b8e295367d5cd1f60043a1c292b
ddpmntcpbr <ddpmntcpbr@gmail.com>
2023-09-03 21:11:29 +0900

    サインインユーザーを取得するアクションを実装

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 end
