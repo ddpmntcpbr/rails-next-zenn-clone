59dc3ce045bbc7916f1f69b39436a6e7b2963354
ddpmntcpbr <ddpmntcpbr@gmail.com>
2023-09-06 22:02:37 +0900

    タスク(backend)の再作成

--- rails/entrypoint.prod.sh
+++ rails/entrypoint.prod.sh
@@ -6,14 +6,14 @@ echo "Start entrypoint.prod.sh"
 echo "rm -f /myapp/tmp/pids/server.pid"
 rm -f /myapp/tmp/pids/server.pid
 
-echo "bundle exec rails db:create RAILS_ENV=production"
-bundle exec rails db:create RAILS_ENV=production
+# echo "bundle exec rails db:create RAILS_ENV=production"
+# bundle exec rails db:create RAILS_ENV=production
 
 echo "bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
 
-echo "bundle exec rails db:seed RAILS_ENV=production"
-bundle exec rails db:seed RAILS_ENV=production
+# echo "bundle exec rails db:seed RAILS_ENV=production"
+# bundle exec rails db:seed RAILS_ENV=production
 
 echo "exec pumactl start"
 bundle exec pumactl start
