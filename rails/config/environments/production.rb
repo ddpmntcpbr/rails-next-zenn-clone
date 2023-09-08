2c469ce3439f5fdaba6d0c22fb5ff3cd24fb4cd5
ddpmntcpbr <ddpmntcpbr@gmail.com>
2023-09-08 22:09:41 +0900

    GmailからSMTPでメール送信

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -78,4 +78,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 認証メール送信に関する記述
+  config.action_mailer.default_options = { from: "no-reply@rails-next-zenn-clone-app.com" }
+  config.action_mailer.default_url_options = { host: "https://rails-next-zenn-clone-app.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: Rails.application.credentials.production.gmail.user_name,
+    password: Rails.application.credentials.production.gmail.password,
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 end
