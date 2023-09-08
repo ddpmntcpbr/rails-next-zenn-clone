198749cd4b5a852c3a21d2c560bf04f5e99a5ace
ddpmntcpbr <ddpmntcpbr@gmail.com>
2023-09-08 22:25:49 +0900

    GithubAction CD設定

--- next/src/pages/api/health_check.ts
+++ next/src/pages/api/health_check.ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ status: 'ok' }))
+  res.end(JSON.stringify({ status: 'ok!' }))
 }
