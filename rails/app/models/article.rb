1095ce0aace5507bce9f692f8ba63929ba3610a6
ddpmntcpbr <ddpmntcpbr@gmail.com>
2024-08-20 20:19:13 +0900

    prefixを削除

--- rails/app/models/article.rb
+++ rails/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  enum :status, { unsaved: 10, draft: 20, published: 30 }, _prefix: true
+  enum :status, { unsaved: 10, draft: 20, published: 30 }
   validates :title, :content, presence: true, if: :published?
   validate :verify_only_one_unsaved_status_is_allowed
 
